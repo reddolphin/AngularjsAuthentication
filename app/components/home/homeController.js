1c6ae323f040bb46933628970925977c6b6fcba9
alex <alechapman@red-dolphin.co.uk>
2015-09-07 18:49:49 +0100

    Authentication

---
+++ app/components/home/homeController.js
@@ -0,0 +1,16 @@
+/**
+ * Created by alex on 06/02/2015.
+ */
+(function(){
+     function HomeController(){
+            var vm = this;
+
+            vm.message=message();
+            vm.title = 'Avengers';
+
+            function message() {
+                return 'This is Show orders screen';
+            }
+        }
+     angular.module('ShopkinsApp').controller('HomeController',[HomeController]);
+})();
